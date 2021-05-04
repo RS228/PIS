87303f72870e8abb628e91e472b84baa91b31743
RS228 <adrobson13@gmail.com>
2021-05-04 23:48:13 +0300

    Server

--- Lab6/Httppshka_server.js
+++
@@ -1,9 +0,0 @@
-const http = require('http');
-const server = http.createServer((req,res)=>{
-    res.end('<h1>End Server</h1>')
-})
-
-
-server.listen(3000,()=>{
-    console.log('You are a really cool programist and you has started server...')
-})
---
+++ Lab6/index.js
@@ -0,0 +1,37 @@
+// const http = require('http');
+// const server = http.createServer((req,res)=>{
+//     res.end('<h1>End Server</h1>')
+// })
+
+
+// server.listen(3000,()=>{
+//     console.log('You are a really cool programist and you has started server...')
+// })
+
+const http = require('http');
+
+const port = + process.env.PORT || 228;
+
+const handler = (req, res) => {
+	console.log(`-> ${req.method} ${req.url}`);
+	const myMoodleUsername = 'is-91-21';
+	if (req.url.slice(1) === myMoodleUsername) {
+		const body = {
+			name: 'Роман',
+			surname: 'Сильчук',
+			course: 2,
+			group: 'ІС-91'
+		};
+        
+		res.setHeader('Content-Type', 'application/json');
+		res.end(JSON.stringify(body));
+	}
+    else{
+        res.end("Bye, bye :)>");
+    }
+};
+
+const server = http.createServer(handler);
+server.listen(port, () => {
+	console.log(`Server is listening on ${port}`);
+});
